abd9b0e1fde46a9a405006122b8a014965e5d967
Eric Quintana <quintanaeric70@gmail.com>
2026-05-15 16:16:55 -0600

    Variable Jump!
    small jump when key is lightly pressed, big jump when key is held down

--- player.gd
+++ player.gd
@@ -18,3 +18,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
+
+func _input(event):
+	if event.is_action_released("Jump"):
+		if !is_on_floor():
+			velocity.y *= 0.5
