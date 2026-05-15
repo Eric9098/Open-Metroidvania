69ef1b718205b959347be8ad5e9fc153cf55c979
Eric Quintana <quintanaeric70@gmail.com>
2026-05-15 16:27:22 -0600

    Jump checked every frame
    The game now checks if the jump key is pressed every frame, to avoid weird stutters and unresponsive jump presses.

--- player.gd
+++ player.gd
@@ -5,7 +5,7 @@ const JUMP_VELOCITY = -700.0
 
 func _physics_process(delta: float) -> void:
 	
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
 	if not is_on_floor():
